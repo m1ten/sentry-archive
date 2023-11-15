34e7933d0f9794b4a0657b8757c98ab097b40f26
miten <57693631+m1ten@users.noreply.github.com>
2023-11-15 12:50:45 -0500

    Update sentry.ts

--- src/cmds/misc/sentry.ts
+++ src/cmds/misc/sentry.ts
@@ -9,12 +9,19 @@ export const data = new SlashCommandBuilder()
     .setDescription('Information about Sentry');
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-    const trueLatency = Math.abs(interaction.client.ws.ping + Date.now() - interaction.createdTimestamp).toPrecision(2)
+    const trueLatency = Math.floor(interaction.client.ws.ping);
     const userCpuUsage = Math.floor(process.cpuUsage().user / 1000); // microseconds to milliseconds
     const systemCpuUsage = Math.floor(process.cpuUsage().system / 1000); // microseconds to milliseconds
     const memoryUsage = Math.floor(process.memoryUsage().rss / 1024 / 1024);
     const uptime = Math.floor(process.uptime());
-
+    // e.g. 1d 2h 3m 4s = 93784 seconds
+    const fmtUptime = {
+        days: Math.floor(uptime / 86400),
+        hours: Math.floor((uptime % 86400) / 3600),
+        minutes: Math.floor((uptime % 3600) / 60),
+        seconds: Math.floor(uptime % 60),
+    };
+    const uptimeString = `${fmtUptime.days}d ${fmtUptime.hours}h ${fmtUptime.minutes}m ${fmtUptime.seconds}s`;
     const owner = interaction.client.users.cache.get(process.env.OWNER_ID)?.tag;
 
     // Embedded message with information about Sentry
@@ -41,14 +48,16 @@ export async function execute(interaction: ChatInputCommandInteraction) {
             },
             {
                 name: 'Uptime',
-                value: `${uptime} seconds`,
+                value: uptimeString,
                 inline: true,
             },
         )
         .setTimestamp(new Date())
         .setFooter({
             text: `Written in TS using discord.js and bun.sh\nMade by @${owner}`,
-            iconURL: interaction.client.users.cache.get(process.env.OWNER_ID)?.displayAvatarURL() as string,
+            iconURL: interaction.client.users.cache
+                .get(process.env.OWNER_ID)
+                ?.displayAvatarURL() as string,
         });
 
     await interaction.reply({
